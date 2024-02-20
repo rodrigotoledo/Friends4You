8b25db1b8f6f972855e229567d347a42e2796089
rtoledo_dev <rodrigo@rtoledo.inf.br>
2024-02-20 13:45:49 -0300

    Add manifest and functions scripts, update application layout

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,5 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../javascripts .js
+//= link jquery.js
+//= link jquery-ui.js
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,14 @@
 class DashboardController < ApplicationController
   def index
   end
-  
+
   def change_user
     User.clear_visit
     @current_user          = User.find(params[:id])
-    session[:current_user] = @current_user.attributes
-    redirect_to :back
+    session[:current_user_id] = @current_user.id
+    redirect_to root_path
   end
-  
+
   def clear_visit
     User.clear_visit
     redirect_to root_path
